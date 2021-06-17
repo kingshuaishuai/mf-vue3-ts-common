d2a3e60d06c50bd467c9c22bf1142ecde594be94
kingshuaishuai <ken.wang@mrs.ai>
2021-06-17 19:12:52 +0800

    优化vue.config.js

--- template/vue.config.js
+++ template/vue.config.js
@@ -2,10 +2,18 @@ const pkg = require('./package.json')
 const deps = pkg.dependencies
 
 module.exports = {
-  // devServer: {
-  //   proxy: ''
-  // },
-  publicPath: './',
+  devServer: {
+    port: <%= customInfo.port %>,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+      "X-Requested-With, content-type, Authorization",
+    },
+    proxy: ''
+  },
+  // publicPath: './',
   productionSourceMap: false,
   pluginOptions: {
     'style-resources-loader': {
@@ -29,15 +37,18 @@ module.exports = {
         return args;
       })
 
-    config
-      .plugin("module-federation-plugin")
-      .use(require("webpack").container.ModuleFederationPlugin, [
-        {
-          library: { type: "var", name: "<%= customInfo.moduleName %>" },
+      config.optimization.delete('splitChunks')
+      /* module federation plugin import */
+      config
+        .plugin('module-federation-plugin')
+        .use(require('webpack').container.ModuleFederationPlugin, [{
+          library: { type: 'var', name: '<%= customInfo.moduleName %>' },
           name: "<%= customInfo.moduleName %>",
           filename: "remoteEntry.js",
           remotes: {},
-          exposes: {},
+          exposes: {
+            // './HelloWorld': './src/components/HelloWorld.tsx'
+          },
           shared: {
             ...deps,
             vue: {
@@ -56,8 +67,7 @@ module.exports = {
               singleton: true,
               requiredVersion: deps["ant-design-vue"],
             }
-          },
-        },
-      ]);
+          }
+      }])
   },
 }
